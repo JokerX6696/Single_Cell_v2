c67ad557aaf4dc8f89e8c986791345e7cafb0f25
JokerX6696 <313445042@qq.com>
2024-02-28 16:23:45 +0800

    防止cellchat 循环

--- single_cell_auto.py
+++ single_cell_auto.py
@@ -77,9 +77,25 @@ def cellchat_run(obj):
     part_list = obj.cellchat.part_list
     out_sc = obj.out
     
-
-    for p in part_list:
-        script_out = "cmd_cellchat_" + part_name + "_" + str(p) + ".sh"
+    if part:
+        for p in part_list:
+            script_out = "cmd_cellchat_" + part_name + "_" + str(p) + ".sh"
+            with open(f"{out_sc}/{script_out}","w") as f:
+                f.write(f"""set -e
+module load OESingleCell/3.0.d
+Rscript /public/scRNA_works/pipeline/scRNA-seq_further_analysis/CellChat_v1.6.1.R \\
+-i {rds} \\
+-f rds \\
+-s {species}  \\
+-c {col} \\
+""")
+                if group_need:
+                    group_vs = "+".join(group_list)
+                    f.write(f"-g {group_type}  \\\n-d {group_vs} \\\n")
+                f.write(f"-q {part_name} \\\n-u {p} \\\n")
+                f.write(f"-o {out}\n")
+    else:
+        script_out = "cmd_cellchat.sh"
         with open(f"{out_sc}/{script_out}","w") as f:
             f.write(f"""set -e
 module load OESingleCell/3.0.d
@@ -92,8 +108,6 @@ Rscript /public/scRNA_works/pipeline/scRNA-seq_further_analysis/CellChat_v1.6.1.
             if group_need:
                 group_vs = "+".join(group_list)
                 f.write(f"-g {group_type}  \\\n-d {group_vs} \\\n")
-            if part:
-                f.write(f"-q {part_name} \\\n-u {p} \\\n")
             f.write(f"-o {out}\n")
 
 
