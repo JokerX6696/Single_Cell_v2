b352788c9c0743f09a2d40ac75273e2c2061e6d4
JokerX6696 <313445042@qq.com>
2024-03-01 12:45:39 +0800

    add input celltype file type

--- single_cell_auto.py
+++ single_cell_auto.py
@@ -122,6 +122,14 @@ def modified_cell_type_run(obj):
     newcelltype_file = obj.modified_cell_type.newcelltype_file
     Modified_col = obj.modified_cell_type.Modified_col
     reduct = obj.modified_cell_type.reduct
+    newcelltype_file_type = obj.modified_cell_type.newcelltype_file_type
+    if newcelltype_file_type == 'tsv':
+        fgf = 'F'
+    elif newcelltype_file_type == 'csv':
+        fgf = 'T'
+    else:
+        print('细胞文件名后缀只能是 tsv 或 csv，请检查 config 文件中 newcelltype_file_type 参数！')
+        exit(1)
     with open(f'{out}/cmd_modified_cell_type.sh',"w") as f:
         f.write(f"""set -e
 module purge && module load OESingleCell/3.0.d
@@ -138,7 +146,7 @@ changecelltype \\
 -C {Modified_col} \\
 --palette customecol2 \\
 --reduct {reduct} \\
--b F
+-b {fgf}
                 """)
 
 def gsva_run(obj):
